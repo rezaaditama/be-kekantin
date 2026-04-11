4d1cc355fb8274d04b99772c6f244f1acc85365a
Reza Aditama <aditamareza03@gmail.com>
2026-04-11 10:44:41 +0700

    public assets

--- api/index.ts
+++ api/index.ts
@@ -17,6 +17,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use("/uploads", express.static('public/uploads'));
 
 app.use('/api/toko', tokoRoutes);
 app.use('/api/menu', menuRoutes);
